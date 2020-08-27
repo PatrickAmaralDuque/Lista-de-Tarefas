9a93f50d14c15c87dc0ad7ca495892357bc32636
Thumendes <arthurmendespereira505@gmail.com>
2020-08-27 13:42:22 -0300

    Initial Commit

---
+++ Lista de Tarefas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lista de Tarefas
+//
+//  Created by COTEMIG on 27/08/20.
+//  Copyright © 2020 Cotemig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
